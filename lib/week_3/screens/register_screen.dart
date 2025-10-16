15109c827d8f21c9e7703f2dfc0efea0003ef0cb
Samuel Sahempa <sahempasamuel10@gmail.com>
2025-10-17 03:51:23 +0800

    fix(week004): sync register with validators utils and resolve breakages

--- lib/week_3/screens/register_screen.dart
+++ lib/week_3/screens/register_screen.dart
@@ -335,7 +335,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                               color: Colors.blue,
                               decoration: TextDecoration.underline,
                             ),
-                          ],
+                          ),
                         ),
                       ],
                     ),
